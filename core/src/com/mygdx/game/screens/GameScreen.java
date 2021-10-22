7bd2ff7cf1ebb8bff5082389f0a4a1c04e26d184
Elio <elliotfaugier@gmail.com>
2021-10-22 17:42:36 +0200

    Work base

---
+++ core/src/com/mygdx/game/screens/GameScreen.java
@@ -0,0 +1,20 @@
+package com.mygdx.game.screens;
+
+import com.badlogic.gdx.ScreenAdapter;
+import com.mygdx.game.ACLGame;
+
+public class GameScreen extends ScreenAdapter {
+
+    private ACLGame game;
+
+    public GameScreen(ACLGame game) {
+        this.game = game;
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+    }
+
+
+}
--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.mygdx.game.ACLGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "ACL GAME 2021 - EKIPAFOND";
+		config.width = 800;
+		config.height = 480;
 		new LwjglApplication(new ACLGame(), config);
 	}
 }
