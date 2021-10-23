0bde83bb02b9c851ef04435ed942073546faa1e6
Elio <elliotfaugier@gmail.com>
2021-10-23 20:16:56 +0200

    Push for Enes

---
+++ core/src/com/mygdx/game/components/TransformComponent.java
@@ -0,0 +1,21 @@
+package com.mygdx.game.components;
+
+import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.math.Vector3;
+
+public class TransformComponent implements Component {
+    private Vector3 position;
+
+    public TransformComponent(Vector3 position) {
+        this.position = position;
+    }
+
+    Vector3 getPosition(){
+        return new Vector3(position);
+    }
+
+    void applyPositionTranslation(Vector3 newPosition, float scalar){
+        this.position.mulAdd(newPosition, scalar);
+    }
+
+}
