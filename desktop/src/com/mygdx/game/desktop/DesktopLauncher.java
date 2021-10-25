5e52a5f44723beef9f0be8db93c8a8b071209d77
badawyjr <a.badawy007@gmail.com>
2021-10-25 17:17:07 +0200

    - Cleaned code

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -9,7 +9,6 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		World world = new World();
-		world.createMap();
 		config.title = "ACL GAME 2021 - EKIPAFOND";
 		config.width = world.getWidth() * 16 * 2;
 		config.height = world.getHeight() * 16 * 2;
