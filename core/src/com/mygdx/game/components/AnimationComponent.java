232c951d8d6cf06b2976b6f53158f78d37440ffa
Paradoxe <gyx18971167768@gmail.com>
2021-11-15 22:05:07 +0100

    sprint 3 Animation Attack version1 -Add attack animation

--- core/src/com/mygdx/game/components/AnimationComponent.java
+++ core/src/com/mygdx/game/components/AnimationComponent.java
@@ -10,12 +10,15 @@ public class AnimationComponent implements Component {
 
     private TextureAtlas atlas;
     private Array<TextureAtlas.AtlasRegion> atlasRegions, atlasRegionsL,atlasRegionsR, atlasRegionsU, atlasRegionsD;
-    public float animTime = 0.2f;
+    public float animTime = 0.3f;
     private Animation animationRight, animationLeft, animationUp, animationDown;
+    private Array<TextureAtlas.AtlasRegion>atlasRegionsAL,atlasRegionsAR, atlasRegionsAU, atlasRegionsAD;
+    private Animation animationAttackRight, animationAttackLeft, animationAttackUp, animationAttackDown;
 
     public AnimationComponent() {
         atlas = new TextureAtlas(Gdx.files.internal("sprites/HeroPack.atlas"));
         atlasRegions= atlas.getRegions();
+        //walk
         atlasRegionsD = new Array<>();
         atlasRegionsU = new Array<>();
         atlasRegionsL = new Array<>();
@@ -24,6 +27,15 @@ public class AnimationComponent implements Component {
         setUpAnim();
         setLeftAnim();
         setRightAnim();
+        //attack
+        atlasRegionsAD = new Array<>();
+        atlasRegionsAU = new Array<>();
+        atlasRegionsAL = new Array<>();
+        atlasRegionsAR = new Array<>();
+        setAttackDownAnim();
+        setAttackUpAnim();
+        setAttackLeftAnim();
+        setAttackRightAnim();
     }
 
     public TextureAtlas getAtlas() {
@@ -33,7 +45,6 @@ public class AnimationComponent implements Component {
     public Array<TextureAtlas.AtlasRegion> getAtlasRegions() {
         return atlasRegions;
     }
-
 
     public Array<TextureAtlas.AtlasRegion> getAtlasRegionsL() {
         return atlasRegionsL;
@@ -67,10 +78,26 @@ public class AnimationComponent implements Component {
         return animationDown;
     }
 
+    public Animation getAnimationAttackRight() {
+        return animationAttackRight;
+    }
+
+    public Animation getAnimationAttackLeft() {
+        return animationAttackLeft;
+    }
+
+    public Animation getAnimationAttackUp() {
+        return animationAttackUp;
+    }
+
+    public Animation getAnimationAttackDown() {
+        return animationAttackDown;
+    }
+
     private void setRightAnim(){
         atlasRegionsR.add(atlas.findRegion("Character without weapon/walk/walk right1.png"));
         atlasRegionsR.add(atlas.findRegion("Character without weapon/walk/walk right2.png"));
-        atlasRegionsR.add(atlas.findRegion("Character without weapon/walk/walk right2.png"));
+        atlasRegionsR.add(atlas.findRegion("Character without weapon/walk/walk right3.png"));
         atlasRegionsR.add(atlas.findRegion("Character without weapon/walk/walk right4.png"));
         animationRight = new Animation(animTime, atlasRegionsR);
         animationRight.setPlayMode(Animation.PlayMode.LOOP);
@@ -99,5 +126,39 @@ public class AnimationComponent implements Component {
         atlasRegionsD.add(atlas.findRegion("Character without weapon/walk/walk down4.png"));
         animationDown = new Animation(animTime, atlasRegionsD);
         animationDown.setPlayMode(Animation.PlayMode.LOOP);
+    }
+
+    private void setAttackRightAnim(){
+        atlasRegionsAR.add(atlas.findRegion("Character with sword and shield/attack/attack right1.png"));
+        atlasRegionsAR.add(atlas.findRegion("Character with sword and shield/attack/attack right2.png"));
+        atlasRegionsAR.add(atlas.findRegion("Character with sword and shield/attack/attack right3.png"));
+        atlasRegionsAR.add(atlas.findRegion("Character with sword and shield/attack/attack right4.png"));
+        animationAttackRight = new Animation(animTime, atlasRegionsAR);
+        animationAttackRight.setPlayMode(Animation.PlayMode.LOOP);
+    }
+
+    private void setAttackLeftAnim(){
+        atlasRegionsAL.add(atlas.findRegion("Character with sword and shield/attack/attack left1.png"));
+        atlasRegionsAL.add(atlas.findRegion("Character with sword and shield/attack/attack left2.png"));
+        atlasRegionsAL.add(atlas.findRegion("Character with sword and shield/attack/attack left3.png"));
+        atlasRegionsAL.add(atlas.findRegion("Character with sword and shield/attack/attack left4.png"));
+        animationAttackLeft = new Animation(animTime, atlasRegionsAL);
+        animationAttackLeft.setPlayMode(Animation.PlayMode.LOOP);
+    }
+    private void setAttackUpAnim(){
+        atlasRegionsAU.add(atlas.findRegion("Character with sword and shield/attack/attack up1.png"));
+        atlasRegionsAU.add(atlas.findRegion("Character with sword and shield/attack/attack up2.png"));
+        atlasRegionsAU.add(atlas.findRegion("Character with sword and shield/attack/attack up3.png"));
+        atlasRegionsAU.add(atlas.findRegion("Character with sword and shield/attack/attack up4.png"));
+        animationAttackUp = new Animation(animTime, atlasRegionsAU);
+        animationAttackUp.setPlayMode(Animation.PlayMode.LOOP);
+    }
+    private void setAttackDownAnim(){
+        atlasRegionsAD.add(atlas.findRegion("Character with sword and shield/attack/attack down1.png"));
+        atlasRegionsAD.add(atlas.findRegion("Character with sword and shield/attack/attack down2.png"));
+        atlasRegionsAD.add(atlas.findRegion("Character with sword and shield/attack/attack down3.png"));
+        atlasRegionsAD.add(atlas.findRegion("Character with sword and shield/attack/attack down4.png"));
+        animationAttackDown = new Animation(animTime, atlasRegionsAD);
+        animationAttackDown.setPlayMode(Animation.PlayMode.LOOP);
     }
 }
--- core/src/com/mygdx/game/systems/AttackSystem.java
+++ core/src/com/mygdx/game/systems/AttackSystem.java
@@ -3,6 +3,7 @@ package com.mygdx.game.systems;
 import com.badlogic.ashley.core.*;
 import com.badlogic.ashley.systems.IteratingSystem;
 import com.badlogic.ashley.utils.ImmutableArray;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.math.Vector3;
 import com.mygdx.game.components.*;
 
@@ -12,15 +13,19 @@ public class AttackSystem extends IteratingSystem {
     private ComponentMapper<DirectionComponent> dm;
     private ComponentMapper<TransformComponent> tm;
     private ComponentMapper<AttackerComponent> am;
-
+    private ComponentMapper<AnimationComponent> anm;
+    private ComponentMapper<TextureComponent> txtm;
     private boolean attack;
     public AttackSystem() {
-        super(Family.all(HealthComponent.class, HeroComponent.class,DirectionComponent.class,TransformComponent.class,AttackerComponent.class).get());
+        super(Family.all(HealthComponent.class, HeroComponent.class,DirectionComponent.class,TransformComponent.class,AttackerComponent.class,AnimationComponent.class,TextureComponent.class).get());
         am = ComponentMapper.getFor(AttackerComponent.class);
         heam = ComponentMapper.getFor(HealthComponent.class);
         herm=ComponentMapper.getFor(HeroComponent.class);
         dm=ComponentMapper.getFor(DirectionComponent.class);
         tm=ComponentMapper.getFor(TransformComponent.class);
+        anm=ComponentMapper.getFor(AnimationComponent.class);
+        txtm=ComponentMapper.getFor(TextureComponent.class);
+
         attack=false;
 
     }
@@ -31,8 +36,9 @@ public class AttackSystem extends IteratingSystem {
         DirectionComponent dc=dm.get(entity);
         TransformComponent tc=tm.get(entity);
         AttackerComponent ac=am.get(entity);
+        AnimationComponent anc=anm.get(entity);
+        TextureComponent txtc = txtm.get(entity);
 
-        herc.setState(herc.STATE_ATTACKING);
         int damage=ac.getDamage();
         int direction=dc.getDirection();
         Vector3 position=tc.getPosition();
@@ -43,13 +49,14 @@ public class AttackSystem extends IteratingSystem {
         ImmutableArray<Entity> entities = this.getEngine().getEntitiesFor(family);
         Vector3 positionTem=new Vector3();
         if(attack) {
+            herc.setState(herc.STATE_ATTACKING);
             switch (direction) {
                 case DirectionComponent.UP:
                     positionTem.set(position.x, position.y + 1, position.z);
                     for (Entity e : entities) {
                         TransformComponent monsterPosition = tm.get(e);
                         Vector3 MP=monsterPosition.getPosition();
-                        if( MP.x>=position.x-20&&MP.x<=position.x+20&&MP.y>=position.y+20&&MP.y<=position.y+40) {
+                        if( MP.x>=position.x-10&&MP.x<=position.x+10&&MP.y>=position.y+10&&MP.y<=position.y+20) {
                             HealthComponent monsterHealth = heam.get(e);
                             System.out.println(monsterHealth.getHealthPoint());
                             monsterHealth.reduceHealthPoint(damage);
@@ -61,7 +68,7 @@ public class AttackSystem extends IteratingSystem {
                     for (Entity e : entities) {
                         TransformComponent monsterPosition = tm.get(e);
                         Vector3 MP=monsterPosition.getPosition();
-                        if (MP.x>=position.x-20&&MP.x<=position.x+20&&MP.y<=position.y-20&&MP.y>=position.y-40) {
+                        if (MP.x>=position.x-10&&MP.x<=position.x+10&&MP.y<=position.y-10&&MP.y>=position.y-20) {
                             HealthComponent monsterHealth = heam.get(e);
                             monsterHealth.reduceHealthPoint(damage);
                         }
@@ -72,7 +79,7 @@ public class AttackSystem extends IteratingSystem {
                     for (Entity e : entities) {
                         TransformComponent monsterPosition = tm.get(e);
                         Vector3 MP=monsterPosition.getPosition();
-                        if (MP.x>=position.x+20&&MP.x<=position.x+40&&MP.y>=position.y-20&&MP.y<=position.y+20) {
+                        if (MP.x>=position.x+10&&MP.x<=position.x+20&&MP.y>=position.y-10&&MP.y<=position.y+10) {
                             HealthComponent monsterHealth = heam.get(e);
                             monsterHealth.reduceHealthPoint(damage);
                         }
@@ -83,7 +90,7 @@ public class AttackSystem extends IteratingSystem {
                     for (Entity e : entities) {
                         TransformComponent monsterPosition = tm.get(e);
                         Vector3 MP=monsterPosition.getPosition();
-                        if (MP.x<=position.x-20&&MP.x>=position.x-40&&MP.y>=position.y-20&&MP.y<=position.y+20) {
+                        if (MP.x<=position.x-10&&MP.x>=position.x-20&&MP.y>=position.y-10&&MP.y<=position.y+10) {
                             HealthComponent monsterHealth = heam.get(e);
                             monsterHealth.reduceHealthPoint(damage);
                         }
