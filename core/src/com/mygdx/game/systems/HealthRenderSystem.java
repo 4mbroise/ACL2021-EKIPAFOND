b835840fea44b6793459e50f8f114ddf6409050a
1907ns <nstwitter19@gmail.com>
2021-11-17 00:36:09 +0100

    Modification of health rendering for camera

--- core/src/com/mygdx/game/systems/HealthRenderSystem.java
+++ core/src/com/mygdx/game/systems/HealthRenderSystem.java
@@ -8,9 +8,16 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.mygdx.game.Assets;
 import com.mygdx.game.components.HealthComponent;
 import com.mygdx.game.components.HeroComponent;
+import com.mygdx.game.screens.MenuScreen;
 
 import javax.xml.soap.Text;
 
@@ -29,13 +36,13 @@ public class HealthRenderSystem extends IteratingSystem {
     private Batch batch;
     private Texture heartTexture;
     private final float fontScale = 0.75f; //static font scale
-
+    private Stage stage;
     /**
      * Constructor
      * @param batch game's batch
      * @param assets game's assets
      */
-    public HealthRenderSystem(Batch batch, Assets assets) {
+    public HealthRenderSystem(Batch batch, Assets assets, Stage stage) {
         super(Family.all(HeroComponent.class, HealthComponent.class).get());
         this.heroMapper = ComponentMapper.getFor(HeroComponent.class);
         this.healthMapper = ComponentMapper.getFor(HealthComponent.class);
@@ -43,6 +50,7 @@ public class HealthRenderSystem extends IteratingSystem {
         this.health =assets.getManager().get("fonts/Minecraft.ttf");
         this.heartTexture = assets.getManager().get("tiles/vie.png");
         this.batch = batch;
+        this.stage=stage;
     }
 
 
@@ -57,7 +65,13 @@ public class HealthRenderSystem extends IteratingSystem {
         health.getData().setScale(fontScale);
         batch.begin();
         for(int i = 0; i<hc.healthPoint;i++){
-            batch.draw(heartTexture, -Gdx.graphics.getWidth()/8, Gdx.graphics.getHeight()/2+resizing);
+            //batch.draw(heartTexture, -Gdx.graphics.getWidth()/8, Gdx.graphics.getHeight()/2+resizing);
+            Button.ButtonStyle homeStyle=new Button.ButtonStyle();
+            //start button
+            homeStyle.up=new TextureRegionDrawable(new TextureRegion(heartTexture));
+            Button heartButton=new Button(homeStyle);
+            heartButton.setPosition(0,0+resizing);
+            stage.addActor(heartButton);
             resizing+=heartTexture.getWidth();
         }
         batch.end();
